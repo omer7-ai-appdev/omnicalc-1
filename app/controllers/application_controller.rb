4b9687a066f51e0700a235dba7aae2a633c236b1
Omero Arreola <omarreola16@gmail.com>
2022-10-12 19:25:37 +0000

    finished square and square root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
 
+    render({ :template => "calculation_templates/square_form.html.erb" })
+  end
+
+  def calculate_square
+    @num = params.fetch("number").to_f
+    @square_of_num = @num **2
+
+  render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def square_root_form
+
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_root
+    @num = params.fetch("user_root").to_f
+    @root_of_num = Math.sqrt(@num) 
+
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 
+  get("/square/new", { :controller => "application", :action => "blank_square_form" })
+
+  get("/square/results", {:controller => "application", :action => "calculate_square"})
+
+  get("/square_root/new", {:controller => "application", :action => "square_root_form"})
+
+  get("/square_root/results", {:controller => "application", :action => "calculate_root"})
+
 end
